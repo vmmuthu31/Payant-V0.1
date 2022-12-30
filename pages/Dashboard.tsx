a87aeee3ca7efd93e08afa8e45214e2e113a206a
Vairamuthu <mvairamuthu2003@gmail.com>
2022-12-30 23:52:10 +0530

    added the dashboard page

--- pages/Dashboard.tsx
+++ pages/Dashboard.tsx
@@ -1,23 +1,25 @@
 import { Fragment, useState } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
+import Image from "next/image";
+import logo from "./Assets/logo.png";
 import {
-  CalendarIcon,
-  ChartBarIcon,
-  FolderIcon,
+  CogIcon,
+  CreditCardIcon,
+  FolderOpenIcon,
   HomeIcon,
-  InboxIcon,
   MenuIcon,
-  UsersIcon,
+  PlusCircleIcon,
+  ShareIcon,
   XIcon,
 } from '@heroicons/react/outline'
 
 const navigation = [
   { name: 'Dashboard', href: '#', icon: HomeIcon, current: true },
-  { name: 'Team', href: '#', icon: UsersIcon, current: false },
-  { name: 'Projects', href: '#', icon: FolderIcon, current: false },
-  { name: 'Calendar', href: '#', icon: CalendarIcon, current: false },
-  { name: 'Documents', href: '#', icon: InboxIcon, current: false },
-  { name: 'Reports', href: '#', icon: ChartBarIcon, current: false },
+  { name: 'Create an invoice', href: '#', icon: PlusCircleIcon, current: false },
+  { name: 'Deliverables', href: '#', icon: FolderOpenIcon, current: false },
+  { name: 'Transactions', href: '#', icon: CreditCardIcon, current: false },
+  { name: 'Settings', href: '#', icon: CogIcon, current: false },
+  { name: 'Refferals', href: '#', icon: ShareIcon, current: false },
 ]
 
 function classNames(...classes) {
@@ -82,11 +84,7 @@ export default function Dashboard() {
                 </Transition.Child>
                 <div className="flex-1 h-0 pt-5 pb-4 overflow-y-auto">
                   <div className="flex-shrink-0 flex items-center px-4">
-                    <img
-                      className="h-8 w-auto"
-                      src="https://tailwindui.com/img/logos/workflow-logo-indigo-600-mark-gray-800-text.svg"
-                      alt="Workflow"
-                    />
+                  <Image className="w-full h-12 mr-2" src={logo} alt="logo" />
                   </div>
                   <nav className="mt-5 px-2 space-y-1">
                     {navigation.map((item) => (
@@ -141,11 +139,7 @@ export default function Dashboard() {
           <div className="flex-1 flex flex-col min-h-0 border-r border-gray-200 bg-white">
             <div className="flex-1 flex flex-col pt-5 pb-4 overflow-y-auto">
               <div className="flex items-center flex-shrink-0 px-4">
-                <img
-                  className="h-8 w-auto"
-                  src="https://tailwindui.com/img/logos/workflow-logo-indigo-600-mark-gray-800-text.svg"
-                  alt="Workflow"
-                />
+              <Image className="w-full h-12  mb-5" src={logo} alt="logo" />
               </div>
               <nav className="mt-5 flex-1 px-2 bg-white space-y-1">
                 {navigation.map((item) => (
@@ -169,23 +163,7 @@ export default function Dashboard() {
                 ))}
               </nav>
             </div>
-            <div className="flex-shrink-0 flex border-t border-gray-200 p-4">
-              <a href="#" className="flex-shrink-0 w-full group block">
-                <div className="flex items-center">
-                  <div>
-                    <img
-                      className="inline-block h-9 w-9 rounded-full"
-                      src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                      alt=""
-                    />
-                  </div>
-                  <div className="ml-3">
-                    <p className="text-sm font-medium text-gray-700 group-hover:text-gray-900">Tom Cook</p>
-                    <p className="text-xs font-medium text-gray-500 group-hover:text-gray-700">View profile</p>
-                  </div>
-                </div>
-              </a>
-            </div>
+            
           </div>
         </div>
         <div className="md:pl-64 flex flex-col flex-1">
