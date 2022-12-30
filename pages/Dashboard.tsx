e637d0c52dd9cc45b7b1d270253af4c69da06e25
Vairamuthu <mvairamuthu2003@gmail.com>
2022-12-31 00:07:20 +0530

    added the wallet button in dashboard page

--- pages/Dashboard.tsx
+++ pages/Dashboard.tsx
@@ -31,13 +31,6 @@ export default function Dashboard() {
 
   return (
     <>
-      {/*
-        This example requires updating your template:
-        ```
-        <html class="h-full bg-gray-100">
-        <body class="h-full">
-        ```
-      */}
       <div>
         <Transition.Root show={sidebarOpen} as={Fragment}>
           <Dialog as="div" className="fixed inset-0 flex z-40 md:hidden" onClose={setSidebarOpen}>
@@ -85,6 +78,7 @@ export default function Dashboard() {
                 <div className="flex-1 h-0 pt-5 pb-4 overflow-y-auto">
                   <div className="flex-shrink-0 flex items-center px-4">
                   <Image className="w-full h-12 mr-2" src={logo} alt="logo" />
+                  
                   </div>
                   <nav className="mt-5 px-2 space-y-1">
                     {navigation.map((item) => (
@@ -110,23 +104,7 @@ export default function Dashboard() {
                     ))}
                   </nav>
                 </div>
-                <div className="flex-shrink-0 flex border-t border-gray-200 p-4">
-                  <a href="#" className="flex-shrink-0 group block">
-                    <div className="flex items-center">
-                      <div>
-                        <img
-                          className="inline-block h-10 w-10 rounded-full"
-                          src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                          alt=""
-                        />
-                      </div>
-                      <div className="ml-3">
-                        <p className="text-base font-medium text-gray-700 group-hover:text-gray-900">Tom Cook</p>
-                        <p className="text-sm font-medium text-gray-500 group-hover:text-gray-700">View profile</p>
-                      </div>
-                    </div>
-                  </a>
-                </div>
+               
               </div>
             </Transition.Child>
             <div className="flex-shrink-0 w-14">{/* Force sidebar to shrink to fit close icon */}</div>
@@ -137,9 +115,10 @@ export default function Dashboard() {
         <div className="hidden md:flex md:w-64 md:flex-col md:fixed md:inset-y-0">
           {/* Sidebar component, swap this element with another sidebar if you like */}
           <div className="flex-1 flex flex-col min-h-0 border-r border-gray-200 bg-white">
-            <div className="flex-1 flex flex-col pt-5 pb-4 overflow-y-auto">
-              <div className="flex items-center flex-shrink-0 px-4">
+            <div className="flex-1 flex flex-col pt-5 m-5 pb-4 overflow-y-auto">
+              <div className="items-center text-center flex-shrink-0 px-4">
               <Image className="w-full h-12  mb-5" src={logo} alt="logo" />
+              <button className=' border-2 pr-8 rounded-md pl-8 border-violet-800'> Connect Wallet</button>
               </div>
               <nav className="mt-5 flex-1 px-2 bg-white space-y-1">
                 {navigation.map((item) => (
@@ -177,7 +156,7 @@ export default function Dashboard() {
               <MenuIcon className="h-6 w-6" aria-hidden="true" />
             </button>
           </div>
-          <main className="flex-1">
+          <main >
             <div className="py-6">
               <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
                 <h1 className="text-2xl font-semibold text-gray-900">Dashboard</h1>
@@ -185,7 +164,7 @@ export default function Dashboard() {
               <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
                 {/* Replace with your content */}
                 <div className="py-4">
-                  <div className="border-4 border-dashed border-gray-200 rounded-lg h-96" />
+                  
                 </div>
                 {/* /End replace */}
               </div>
