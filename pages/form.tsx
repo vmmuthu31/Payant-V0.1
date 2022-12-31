ae0d3f89e6ac0ab1ff0745b2b6ef2841da5331ad
Vairamuthu <mvairamuthu2003@gmail.com>
2022-12-31 17:09:43 +0530

    added the buttons to the setting cards

--- pages/form.tsx
+++ pages/form.tsx
@@ -38,8 +38,8 @@ export default function Form() {
                       <textarea  name="name" id="name" className=" border border-gray-300 text-black sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full h-32 p-2.5   dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Scope of work" required />
                   </div>
                   <div className="flex">
-                      <input type="date" name="name" id="name" placeholder="Start Date" className=" border border-gray-300 text-black sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
-                      <input type="date" name="name" id="name" placeholder="End Date" className=" border border-gray-300 text-black sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                      <input type="date" name="name" id="name" value="Start Date" className=" border border-gray-300 text-black sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                      <input type="date" name="name" id="name" value="End Date" className=" border border-gray-300 text-black sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
                   </div>
               <Link href="#">    <button type="submit" className="w-full mt-4 text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Next</button></Link>
                 
