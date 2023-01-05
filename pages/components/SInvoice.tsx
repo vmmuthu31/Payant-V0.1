c72653b78853977b94db34440eb1f1d65bbb1869
Vairamuthu <mvairamuthu2003@gmail.com>
2023-01-05 19:53:03 +0530

    updated the Invoices

--- pages/components/SInvoice.tsx
+++ pages/components/SInvoice.tsx
@@ -2,19 +2,40 @@ import React from "react";
 
 const SInvoice = () => {
   return (
-    <div className="form">
+    <div className="form space-y-2">
     <input
       type="text"
       name="fullname"
-      placeholder="Steve Jobs"
-      className="border border-gray-300 text-black sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"
+      placeholder="Invoice's Name"
+      className="border border-gray-300 text-black sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full  p-2.5  dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"
     />
-    <input
-      type="text"
+    <div className="flex gap-3"><button>Item #1</button> <button className="bg-purple-500 rounded-md text-sm text-white p-2">Add more items</button></div>
+    <textarea
       name="displayName"
-      placeholder="Steve"
+      placeholder="Scope of work"
+      className="border border-gray-300 text-black sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 h-40  dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"
+    />
+    <div className="flex gap-5">
+     <input
+      type="text"
+      name="fullname"
+      placeholder="Start Date (Optional)"
       className="border border-gray-300 text-black sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"
     />
+     <input
+      type="text"
+      name="fullname"
+      placeholder="End Date (Optional)"
+      className="border border-gray-300 text-black sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"
+    />
+    </div>
+     <input
+      type="text"
+      name="fullname"
+      placeholder="Price"
+      className="border border-gray-300 text-black sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"
+    />
+
   </div>
   );
 };
