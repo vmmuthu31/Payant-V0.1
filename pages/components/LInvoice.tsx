030e919c8ccef1ec9897a1b44e93e44a650c640f
Vairamuthu <mvairamuthu2003@gmail.com>
2023-01-05 20:07:49 +0530

    updated the Invoices

--- pages/components/LInvoice.tsx
+++ pages/components/LInvoice.tsx
@@ -2,11 +2,11 @@ import { BadgeCheckIcon } from "@heroicons/react/outline";
 import React from "react";
 
 const LInvoice = () => {
-  return <>
+  return <div className="space-y-3">
   <BadgeCheckIcon className="text-violet-600 mx-auto h-12" />
   <h1 className="text-violet-600 text-center text-xl bold">Invoice Created</h1>
-  <p className="text-center "> The deliverables have been successfully reviewed by [client_name]. The funds have been unlocked</p>
-  
-  </>;
+  <p className="text-center "> Your Smart Invoice for [client_name] has been sent and a copy has been emailed to you.</p>
+  <p className="text-center ">We&lsquo;ll let you know as soon as the payment is locked in Payant.</p>
+  </div>;
 };
 export default LInvoice;
