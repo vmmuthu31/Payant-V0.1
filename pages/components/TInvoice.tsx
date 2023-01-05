3b8bdab0ef921e9ec25013f6359879279bef381d
Vairamuthu <mvairamuthu2003@gmail.com>
2023-01-05 19:55:56 +0530

    updated the Invoices

--- pages/components/TInvoice.tsx
+++ pages/components/TInvoice.tsx
@@ -2,17 +2,29 @@ import React from "react";
 
 const TInvoice = () => {
   return (
-    <div className="form">
+    <div className="form space-y-3">
       <input
         type="text"
         name="fullname"
-        placeholder="Steve Jobs"
+        placeholder="Client's Name"
         className="border border-gray-300 text-black sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"
       />
       <input
         type="text"
         name="displayName"
-        placeholder="Steve"
+        placeholder="Invoice's Name"
+        className="border border-gray-300 text-black sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        />
+        <input
+        type="text"
+        name="displayName"
+        placeholder="Total Amount"
+        className="border border-gray-300 text-black sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        />
+        <input
+        type="text"
+        name="displayName"
+        placeholder="Payat's Fee"
         className="border border-gray-300 text-black sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"
         />
     </div>
