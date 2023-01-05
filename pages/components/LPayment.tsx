8ac581c4bb53f971c9dff8a3ece7069da5b6175b
Vairamuthu <mvairamuthu2003@gmail.com>
2023-01-05 20:50:15 +0530

    updated the payment pages

--- pages/components/LPayment.tsx
+++ pages/components/LPayment.tsx
@@ -2,11 +2,11 @@ import { BadgeCheckIcon } from "@heroicons/react/outline";
 import React from "react";
 
 const LastPayment = () => {
-  return <>
+  return <div className="space-y-3">
   <BadgeCheckIcon className="text-violet-600 mx-auto h-12" />
   <h1 className="text-violet-600 text-center text-xl bold">Funds Unlocked</h1>
-  <p className="text-center "> The deliverables have been successfully reviewed by [client_name]. The funds have been unlocked</p>
+  <p className="text-center">The deliverables have been successfully reviewed by [client_name]. The funds have been unlocked</p>
   
-  </>;
+  </div>;
 };
 export default LastPayment;
