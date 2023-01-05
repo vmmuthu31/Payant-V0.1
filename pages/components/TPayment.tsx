55b1d2ea3ec952c2c2b232d9426d867423ac2df2
Vairamuthu <mvairamuthu2003@gmail.com>
2023-01-05 20:47:30 +0530

    updated the payment pages

--- pages/components/TPayment.tsx
+++ pages/components/TPayment.tsx
@@ -7,7 +7,8 @@ const ThirdPayment = () => {
     <div className="form space-y-3">
 <DocumentAddIcon className="text-violet-600 mx-auto h-12" />
   <h1 className="text-violet-600 text-center text-xl bold">Under Review</h1>
-  <p className="text-center "> The deliverables have been successfully reviewed by [client_name]. The funds have been unlocked</p>
+  <p className="text-center"> The deliverables are being reviewed by [client_name]. They have until [insert_date] to review them.</p>
+  <p className="text-center">Passed this period the funds will be automatically unlocked. </p>
       </div>
   );
 };
