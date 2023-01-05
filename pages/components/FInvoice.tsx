e596839af2781536c6933e04316aca003406e426
Vairamuthu <mvairamuthu2003@gmail.com>
2023-01-05 19:21:31 +0530

    updated the progressive payments

--- pages/components/FInvoice.tsx
+++ pages/components/FInvoice.tsx
@@ -7,12 +7,8 @@ const FInvoice= () => {
         placeholder="Steve Jobs"
         className="border border-gray-300 text-black sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"
       />
-      <input
-        type="text"
-        name="displayName"
-        placeholder="Steve"
-        className="border border-gray-300 text-black sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"
-      />
+      <select className="border border-gray-300 text-black sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"></select>
+        
     </div>
   );
 };
