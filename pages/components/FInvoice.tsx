611da55f938efa47d227b3d55c9f0be0e5ffb044
Vairamuthu <mvairamuthu2003@gmail.com>
2023-01-05 19:30:42 +0530

    updated the Invoices

--- pages/components/FInvoice.tsx
+++ pages/components/FInvoice.tsx
@@ -1,13 +1,24 @@
+import Link from "next/link";
+
 const FInvoice= () => {
   return (
     <div className="form">
       <input
         type="text"
         name="fullname"
-        placeholder="Steve Jobs"
+        placeholder="Invoice's Name"
         className="border border-gray-300 text-black sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"
       />
-      <select className="border border-gray-300 text-black sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"></select>
+    <Link href="/Clientform">  <p className="text-end text-md text-purple-600 pt-2 bold">New Client</p></Link>
+      <select className="border border-gray-300 text-black sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500">
+        <option>Client</option>
+        <option>Client Name 1</option>
+        <option>Client Name 1</option>
+        <option>Client Name 1</option>
+        <option>Client Name 1</option>
+        <option>Client Name 1</option>
+        <option>Client Name 1</option>
+      </select>
         
     </div>
   );
