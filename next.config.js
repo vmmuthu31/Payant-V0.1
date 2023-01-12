73ee05bb64f22cb8cc8881d79cd2c3e967276fde
Vairamuthu <mvairamuthu2003@gmail.com>
2023-01-13 00:04:28 +0530

    updated the blockchain.services.js

--- next.config.js
+++ next.config.js
@@ -34,3 +34,10 @@ module.exports = {
     ignoreBuildErrors: true,
   },
 };
+module.exports = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+};
